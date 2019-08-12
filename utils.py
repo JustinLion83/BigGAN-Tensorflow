2600b1c09a3c7084d8c33085cd9e1cd78e9d269f
JustinLion83 <45571021+JustinLion83@users.noreply.github.com>
2019-08-12 09:43:50 +0800

    Update utils.py

--- utils.py
+++ utils.py
@@ -15,7 +15,7 @@ class ImageData:
         self.custom_dataset = custom_dataset
 
     def image_processing(self, filename):
-
+    # 圖片前處理, 將圖片resize到[load_size, load_size] -> 接著把pixel_value 縮放到[-1.0, 1.0]
         if not self.custom_dataset :
             x_decode = filename
         else :
@@ -42,6 +42,7 @@ def load_cifar10() :
     return x
 
 def load_data(dataset_name) :
+    # 看是要載入甚麼資料庫 mnist 或 cifar10 或 自己的Dataset
     if dataset_name == 'mnist' :
         x = load_mnist()
     elif dataset_name == 'cifar10' :
@@ -54,6 +55,7 @@ def load_data(dataset_name) :
 
 
 def preprocessing(x, size):
+    # 可用cv2取代
     x = scipy.misc.imread(x, mode='RGB')
     x = scipy.misc.imresize(x, [size, size])
     x = normalize(x)
@@ -66,15 +68,21 @@ def save_images(images, size, image_path):
     return imsave(inverse_transform(images), size, image_path)
 
 def merge(images, size):
+    '''把output_img合併成大圖並輸出(我有自己的方法)'''
+    # 檢查最後的channel是 3或4或1
     h, w = images.shape[1], images.shape[2]
+    # 彩色圖
     if (images.shape[3] in (3,4)):
         c = images.shape[3]
-        img = np.zeros((h * size[0], w * size[1], c))
+        img = np.zeros((h * size[0], w * size[1], c)) # 全黑的大圖
         for idx, image in enumerate(images):
-            i = idx % size[1]
-            j = idx // size[1]
+            # 記憶點: 是否"連續變動" (Continuous Change)
+            i = idx % size[1]     # column (每次都+1)        連續變動
+            j = idx // size[1]    # row    (每size次才會+1)  非連續變動
             img[j * h:j * h + h, i * w:i * w + w, :] = image
         return img
+    
+    # 灰度圖
     elif images.shape[3]==1:
         img = np.zeros((h * size[0], w * size[1]))
         for idx, image in enumerate(images):
@@ -110,22 +118,25 @@ def str2bool(x):
 # Regularization
 ##################################################################################
 
+# 用在卷積
 def orthogonal_regularizer(scale) :
     """ Defining the Orthogonal regularizer and return the function at last to be used in Conv layer as kernel regularizer"""
 
     def ortho_reg(w) :
         """ Reshaping the matrxi in to 2D tensor for enforcing orthogonality"""
         _, _, _, c = w.get_shape().as_list()
-
+        # 把batch_size, w, z 都壓縮成一個數字 
         w = tf.reshape(w, [-1, c])
 
         """ Declaring a Identity Tensor of appropriate size"""
+        # 對角線1, 其他元素為0
         identity = tf.eye(c)
 
         """ Regularizer Wt*W - I """
-        w_transpose = tf.transpose(w)
-        w_mul = tf.matmul(w_transpose, w)
-        reg = tf.subtract(w_mul, identity)
+        # 移除w(權重)對角線上的元素
+        w_transpose = tf.transpose(w)       # W的轉置(Wt)
+        w_mul = tf.matmul(w_transpose, w)   # Wt*W
+        reg = tf.subtract(w_mul, identity)  # Wt*W - I
 
         """Calculating the Loss Obtained"""
         ortho_loss = tf.nn.l2_loss(reg)
@@ -134,6 +145,7 @@ def orthogonal_regularizer(scale) :
 
     return ortho_reg
 
+# 用在Dense
 def orthogonal_regularizer_fully(scale) :
     """ Defining the Orthogonal regularizer and return the function at last to be used in Fully Connected Layer """
 
